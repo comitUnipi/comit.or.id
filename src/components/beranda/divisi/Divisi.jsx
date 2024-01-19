1e9c6b1e7da4db2ce82c2490decf5adfd2213827
mupid ahmadiawan <133787157+Yuefii@users.noreply.github.com>
2024-01-19 22:14:55 +0700

    refactor: memperbaiki responsive components divisi dan mentor (#4)

--- src/components/beranda/divisi/Divisi.jsx
+++ src/components/beranda/divisi/Divisi.jsx
@@ -2,18 +2,17 @@ import DivisiCard from "./DivisiCard";
 
 const Divisi = () => {
   return (
-    <section className="text-center mt-5 mx-20">
-      <h3 className="text-4xl font-bold">
+    <div className="text-center mt-10 mx-20">
+      <h3 className="text-4xl lg:text-[48px] font-semibold">
         Pelatihan di <span className="text-primary">Comit</span>
       </h3>
-      <p className="text-gray-600 leading-4 mt-3">
+      <p className="opacity-80 text-md lg:text-[20px] mt-3">
         Pelatihan di comit ada 3 divisi Desain <br /> Grafis, Programming, Comp
         & <br />
         Network
       </p>
-
       <DivisiCard />
-    </section>
+    </div>
   );
 };
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,15 +6,16 @@ import TentangKami from "@/components/beranda/TentangKami";
 const Home = () => {
   return (
     <>
-    <div>
+      <section>
         <Pendahuluan />
         <TentangKami />
-    </div>
-    <div>
-      <Divisi />
-      <Mentor />
-      <div className="bg-primary h-[15px] md:h-[20px] mb-5 mt-5" />
-    </div>
+      </section>
+      <section>
+        <Divisi />
+        <Mentor />
+      </section>
     </>
-    
+  );
+};
+
 export default Home;
