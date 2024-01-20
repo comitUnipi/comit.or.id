b1680094e3742c8303eec38622182d08f314e369
Muhamad Mupid Ahmadiawan <133787157+Yuefii@users.noreply.github.com>
2024-01-20 10:20:59 +0700

    feat: menambahkan navbar & footer (#5)
    * menambahkan package react-icons
    
    * feat: menambahkan on-boarding & tentang kami (#1)
    
    * feat: menambahkan divisi dan mentor (#3)
    
    * refactor: memperbaiki responsive components divisi dan mentor (#4)
    
    * feat: menambahkan navbar & footer
    
    ---------
    
    Co-authored-by: Sholikul Ardian <116469201+sholikul92@users.noreply.github.com>

---
+++ src/components/beranda/mentor/MentorCard.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import Image from "next/image";
+import mentor from "@/helper/mentor/mentor";
+import Autoplay from "embla-carousel-autoplay";
+import {
+  Carousel,
+  CarouselItem,
+  CarouselContent,
+  CarouselNext,
+  CarouselPrevious,
+} from "../../ui/carousel";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+} from "../../ui/card";
+import { CiLinkedin } from "react-icons/ci";
+import { PiInstagramLogoLight } from "react-icons/pi";
+import { CiMail } from "react-icons/ci";
+
+const MentorCard = () => {
+  const plugin = React.useRef(
+    Autoplay({ delay: 2000, stopOnInteraction: true })
+  );
+
+  return (
+    <div>
+      <Carousel
+        className="w-full max-w-4xl mx-auto mt-10"
+        plugins={[plugin.current]}
+        onMouseEnter={plugin.current.stop}
+        onMouseLeave={plugin.current.reset}
+      >
+        <CarouselContent className="-ml-1">
+          {mentor.map((item, index) => (
+            <CarouselItem
+              key={index}
+              className="pl-1 md:basis-1/2 lg:basis-1/3"
+            >
+              <div className="p-1 flex justify-center gap-10">
+                <Card className="bg-gray-200">
+                  <CardHeader>
+                    <Image
+                      src={item.profile}
+                      alt=""
+                      width={100}
+                      height={100}
+                      className="mx-auto border border-gray-800 rounded-full"
+                    />
+                  </CardHeader>
+                  <CardContent>
+                    <CardTitle>{item.name}</CardTitle>
+                    <p className="mt-2  text-primary">{item.desc}</p>
+                  </CardContent>
+                  <CardFooter>
+                    <span className="flex mx-auto gap-2">
+                      <PiInstagramLogoLight className="text-2xl" />
+                      <CiLinkedin className="text-2xl" />
+                      <CiMail className="text-2xl" />
+                    </span>
+                  </CardFooter>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
+};
+
+export default MentorCard;
---
+++ src/components/footer/Atribut.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Link from "next/link";
+import { NavbarList } from "@/helper/navigation/navbarList";
+import { SosmedList } from "@/helper/navigation/sosmedList";
+
+const Atribut = () => {
+  return (
+    <>
+      <div className="flex gap-6">
+        <div className="text-white">
+          <h1 className="opacity-80">Organisasi</h1>
+          {NavbarList.map((item, index) => (
+            <li className="text-xs hover:underline" key={index}>
+              <Link href={item.href}>{item.label}</Link>
+            </li>
+          ))}
+        </div>
+        <div className="text-white">
+          <h1 className="opacity-80">Sosial Media Kami</h1>
+          {SosmedList.map((item, index) => (
+            <li className="text-xs hover:underline" key={index}>
+              <Link href={item.href}>{item.label}</Link>
+            </li>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Atribut;
