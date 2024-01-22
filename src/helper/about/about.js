7d62d11eb93c0c5819bcd1a65252df601650ab48
Sholikul Ardian <116469201+sholikul92@users.noreply.github.com>
2024-01-22 17:10:39 +0700

    feat: menambahkan halaman tentang kami (#15)

---
+++ src/helper/about/about.js
@@ -0,0 +1,26 @@
+const aboutMe = [
+    {
+        id: 1,
+        judul: "Tentang Kami",
+        desc: "Sebagai mahasiswa yang sadar akan panggilan ilmu pengetahuan dan perkembangan teknologi serta mengupayakan penerapan etika Ilmu Pengetahuan dan Tri Dharma Perguruan Tinggi. Menyadari akan tanggung jawab itu maka kami mahasiswa Kampus Insan Pembangunan berkewajiban membina diri agar menjadi bangsa yang memiliki kemampuan akademik dan profesi, sehingga dapat menerapkan ilmu pengetahuan dan mengembangkan teknologi. Oleh karena itu kami menghimpun diri dalam suatu wadah Organisasi dibidang Teknologi, dengan nama “ Community of Information Technology “ pada tanggal 14 Februari 2010."
+    }]
+
+const visiMisi = [
+    {
+        judul: "Visi Kami",
+        desc: [
+            "Melaksanakan berbagai kegiatan yang bermanfaat dan yang bernilai positif.",
+            "Menjalin kerjasama dengan berbagai pihak dalam bidang teknologi informasi."
+        ]
+    },
+    {
+        judul: "Misi Kami",
+        desc: [
+            "Menghasilkan anggota COMIT yang handal dalam dunia informasi.",
+            "Memajukan dan mengembangkan almamater.",
+            "Menjadikan organisasi COMIT, sebagai organisasi yang unggul baik secara akademis maupun non akademis."
+        ]
+    }
+]
+
+export {aboutMe, visiMisi}
