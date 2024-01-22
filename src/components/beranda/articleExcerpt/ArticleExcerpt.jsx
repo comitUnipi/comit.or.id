93070c50ab4ff2a497da009a48ea5f7db63c3392
sholikul92 <sholikulardian@gmail.com>
2024-01-22 10:10:22 +0700

    refactor: mengubah resource artikel dan mereusable komponen tag

--- src/components/beranda/articleExcerpt/ArticleExcerpt.jsx
+++ src/components/beranda/articleExcerpt/ArticleExcerpt.jsx
@@ -1,45 +1,43 @@
-import Link from "next/link"
+import Link from "next/link";
 import Excerpt from "./Excerpt";
-import articleExcerpt from "@/helper/articleExcerpt/articleExcerpt";
+import { KontenList } from "@/helper/artikel/kontenList";
 
 const ArticleExcerpt = () => {
   return (
-    <div className="items-center lg:mx-[10em] mt-2 mx-5">
+    <div className="items-center lg:mx-[10em] mt-2 mx-7">
       <div className="md:flex md:justify-between text-center">
         <h1 className="font-semibold md:text-[48px] text-4xl">Artikel Kami</h1>
-        <p className=" hidden md:block self-end underline md:text-[16px] text-sm">
+        <Link href="/artikel" className=" hidden md:block self-end hover:underline md:text-[16px] text-sm">
           Selengkapnya
-        </p>
+        </Link>
       </div>
       <div>
-        {articleExcerpt.map((value, index) => {
+        {KontenList.slice(0, 3).map((konten, index) => {
           return index % 2 == 0 ? (
             <Excerpt
               key={index}
-              title={value.title}
-              createdAt={value.created_at}
-              tag={value.tag}
-              excerpt={value.excerpt}
-              featuredImage={value.featured_image}
+              title={konten.judul}
+              createdAt={konten.tanggal}
+              tag={konten.topik}
+              excerpt={konten.subJudul}
+              featuredImage={konten.img}
             />
           ) : (
             <Excerpt
               key={index}
-              title={value.title}
-              createdAt={value.created_at}
-              tag={value.tag}
-              excerpt={value.excerpt}
-              featuredImage={value.featured_image}
+              title={konten.judul}
+              createdAt={konten.tanggal}
+              tag={konten.topik}
+              excerpt={konten.subJudul}
+              featuredImage={konten.img}
               rowStart="lg:row-start-1"
             />
           );
         })}
       </div>
       <p className="mt-6 block md:hidden underline text-center pr-4 text-sm">
-        <Link href="/artikel">
-        Selengkapnya
-        </Link>
-        </p>
+        <Link href="/artikel">Selengkapnya</Link>
+      </p>
     </div>
   );
 };
