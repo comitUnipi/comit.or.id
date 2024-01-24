b6a288e21254814027e6e5b8722c19926edff732
Sholikul Ardian <sholikulardian@gmail.com>
2024-01-24 20:19:49 +0700

    feat: menambahkan halaman galeri

---
+++ src/pages/galeri/index.jsx
@@ -0,0 +1,12 @@
+import GaleriList from "@/components/galeri/galeriList";
+
+const galeri = () => {
+  return (
+    <>
+      <h1 className="text-center font-bold text-4xl">Galeri</h1>
+      <GaleriList />
+    </>
+  );
+};
+
+export default galeri;
