19084c867d692c827505400d18e16b4408db4e86
Yuefii <muffidahmadiawan31@gmail.com>
2024-01-24 21:23:17 +0700

    rafactor: fix beberapa components

--- src/components/galeri/galeriList.jsx
+++ src/components/galeri/galeriList.jsx
@@ -10,17 +10,17 @@ const GaleriList = () => {
   return (
     <>
       {galeri.map((item, index) => (
-        <div key={index} className="lg:ml-[3rem] my-10 ml-[2.5rem]">
-          <h2 className="lg:ml-[9rem] font-semibold lg:text-2xl text-xl">
-            {item.judul}
-          </h2>
+        <div key={index} className="mx-[2rem] sm:mx-4 my-10">
           <div className="">
-            <Carousel className="w-full lg:max-w-5xl lg:mx-auto mt-5">
+            <Carousel className="lg:max-w-5xl mx-auto mt-5">
+              <h2 className="font-semibold mb-3 lg:text-2xl text-xl">
+                {item.judul}
+              </h2>
               <CarouselContent className="-ml-1">
                 {item.src.map((image) => (
                   <CarouselItem
                     key={image.id}
-                    className="pl-1 md:basis-1/2 lg:basis-1/3"
+                    className="pl-1 basis-1/2 md:basis-1/3 lg:basis-1/4"
                   >
                     <div className="">
                       <Image src={image.src} alt="" width={300} height={100} />
