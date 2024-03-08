136b83e0bc11c548945464212af945be99f327be
Yuefii <muffidahmadiawan31@gmail.com>
2024-03-08 09:28:38 +0700

    setup: merubah database ke mysql dan menambahkan api login/register

--- src/pages/api/auth/register.js
+++ src/pages/api/auth/register.js
@@ -1,28 +1,47 @@
-import { signUp } from "@/services/firebase_auth"
-
+import bcrypt from "bcrypt";
+import prisma from "@/lib/prisma"
 
 export default async function handler(req, res) {
-    if (req.method === "POST") {
-        await signUp(req.body, (status) => {
-            if (status) {
-                res.status(200).json({
-                    status: true,
-                    statusCode: 200,
-                    message: "success"
-                })
-            } else {
-                res.status(400).json({
-                    status: false,
-                    statusCode: 400,
-                    message: "failed"
-                })
-            }
-        })
-    } else {
-        res.status(405).json({
-            status: false,
-            statusCode: 405,
-            message: "method not allowed"
-        })
+  if (req.method === "POST") {
+    const { nama_lengkap, npm, password, divisi } = req.body;
+    if (!npm || !password) {
+      return res
+        .status(400)
+        .json({ error: "npm, password, and email are required." });
     }
+
+    const existingUser = await prisma.user.findUnique({
+      where: { npm },
+    });
+
+    if (existingUser) {
+      return res
+      .status(400)
+      .json({ error: "npm already exists." });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        nama_lengkap,
+        npm,
+        password: hashedPassword,
+        divisi: {
+          create: divisi,
+        },
+      },
+      include: {
+        divisi: true,
+      },
+    });
+
+    return res
+      .status(201)
+      .json({ message: "User created successfully.", data: newUser });
+  } else {
+    return res
+    .status(405)
+    .json({ error: "Method Not Allowed" });
+  }
 }
